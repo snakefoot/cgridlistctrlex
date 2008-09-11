e771e81d7dd58ecfcb095428dead5f4a73d554f5
sweaty1 <sweaty1@ea00157d-1f56-0410-8c63-f1771a551224>
2008-09-11 10:14:47 +0000

    Fixed pragma manifestdependency

--- trunk/DemoApplication/stdafx.h
+++ trunk/DemoApplication/stdafx.h
@@ -7,7 +7,7 @@
 #pragma warning(disable:4100)	// unreferenced formal parameter
 
 #if _MSC_VER >= 1400
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='' publicKeyToken='6595b64144ccf1df' language=''\"")*
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
 #ifndef VC_EXTRALEAN
