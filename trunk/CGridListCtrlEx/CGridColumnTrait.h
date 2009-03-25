eddcef3f2ff85421e202650d3af0384a08303856
sweaty1 <sweaty1@ea00157d-1f56-0410-8c63-f1771a551224>
2009-03-25 21:50:18 +0000

    Doxygen documentation

--- trunk/CGridListCtrlEx/CGridColumnTrait.h
+++ trunk/CGridListCtrlEx/CGridColumnTrait.h
@@ -15,6 +15,7 @@ class CGridListCtrlEx;
 class CGridColumnTrait
 {
 public:
+	//! Destructor
 	virtual ~CGridColumnTrait() {}
 
 	//! Override OnCustomDraw() to provide your own special cell-drawing
@@ -22,12 +23,19 @@ public:
 	//!   - Must handle focus drawing
 	//!	  - Must handle selection drawing when the CListCtrl doesn't have focus
 	//!   - Must query owner if special foreground/background-color or font should be used
+	//! @param owner The list control drawing
+	//! @param pLVCD Pointer to NMLVCUSTOMDRAW structure
+	//! @param pResult Modification to the drawing stage (CDRF_NEWFONT, etc.)
 	virtual void OnCustomDraw(CGridListCtrlEx& owner, NMLVCUSTOMDRAW* pLVCD, LRESULT* pResult) {}
 
 	//! Override OnEditBegin() to provide your own special cell-edit control
 	//!   - The edit control must inherit from CWnd
 	//!   - The edit control must delete itself when it looses focus
 	//!   - The edit control must send a LVN_ENDLABELEDIT message when edit is complete
+	//! @param owner The list control starting edit
+	//! @param nRow The index of the row
+	//! @param nCol The index of the column
+	//! @return Pointer to the cell editor to use (NULL if cell edit is not possible)
 	virtual CWnd* OnEditBegin(CGridListCtrlEx& owner, int nRow, int nCol) { return NULL; }
 
 	//! Override OnEditEnd() in case one need to change state after a cell-edit
@@ -50,18 +58,17 @@ public:
 			,m_Resizable(true)
 			,m_MetaFlags(0)
 		{}
-		bool m_Visible;
-		int  m_OrgWidth;	// Width it had before being hidden
-		int  m_OrgPosition;	// Position it had before being hidden
-		bool m_AlwaysHidden;
-		bool m_Sortable;
-		bool m_Editable;
-		bool m_Resizable;
+		bool m_Visible;		//!< Column is visible or not
+		int  m_OrgWidth;	//!< Width it had before being hidden
+		int  m_OrgPosition;	//!< Position it had before being hidden
+		bool m_AlwaysHidden;//!< Column can never be visible
+		bool m_Sortable;	//!< Rows can be sorted according to column
+		bool m_Editable;	//!< Cells in the column can be edited
+		bool m_Resizable;	//!< Column width is resizable
 
 		//! Meta-Flags (and data) can be used to store extra properties for a column
 		//! when deriving from CGridListCtrlEx.
 		DWORD m_MetaFlags;
-		CSimpleMap<CString,CString> m_MetaData;
 	};
 	inline ColumnState& GetColumnState() { return m_ColumnState; }
 
