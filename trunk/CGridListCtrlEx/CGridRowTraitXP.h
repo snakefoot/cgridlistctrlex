9f7a7441cadbca726a106eedf200f23dd9ffa615
sweaty1 <sweaty1@ea00157d-1f56-0410-8c63-f1771a551224>
2009-03-26 00:32:07 +0000

    Doxygen documentation

--- trunk/CGridListCtrlEx/CGridRowTraitXP.h
+++ trunk/CGridListCtrlEx/CGridRowTraitXP.h
@@ -1,16 +1,17 @@
 #pragma once
 
-#include "CGridRowTraitText.h"
-
 //------------------------------------------------------------------------
 // Author:  Rolf Kristensen	
 // Source:  http://www.codeproject.com/KB/list/CGridListCtrlEx.aspx
 // License: Free to use for all (New BSD License)
-//
+//------------------------------------------------------------------------
+
+#include "CGridRowTraitText.h"
+
+//------------------------------------------------------------------------
 //! CGridRowTraitXP fixes drawing of rows when the application is using
 //! classic- or XP-style.
 //------------------------------------------------------------------------
-
 class CGridRowTraitXP : public  CGridRowTraitText
 {
 public:
