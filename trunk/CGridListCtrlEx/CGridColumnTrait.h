ad3062ca43373c74d054d4f495419df086e3d6a8
sweaty1 <sweaty1@ea00157d-1f56-0410-8c63-f1771a551224>
2009-03-26 09:08:17 +0000

    Doxygen documentation

--- trunk/CGridListCtrlEx/CGridColumnTrait.h
+++ trunk/CGridListCtrlEx/CGridColumnTrait.h
@@ -18,30 +18,32 @@ public:
 	//! Destructor
 	virtual ~CGridColumnTrait() {}
 
-	//! Override OnCustomDraw() to provide your own special cell-drawing
+	//! Override OnCustomDraw() to provide your own special cell-drawing.
 	//!   - Must handle selection drawing
 	//!   - Must handle focus drawing
 	//!	  - Must handle selection drawing when the CListCtrl doesn't have focus
 	//!   - Must query owner if special foreground/background-color or font should be used
+	//!
 	//! @param owner The list control drawing
 	//! @param pLVCD Pointer to NMLVCUSTOMDRAW structure
 	//! @param pResult Modification to the drawing stage (CDRF_NEWFONT, etc.)
 	virtual void OnCustomDraw(CGridListCtrlEx& owner, NMLVCUSTOMDRAW* pLVCD, LRESULT* pResult) {}
 
-	//! Override OnEditBegin() to provide your own special cell-edit control
+	//! Override OnEditBegin() to provide your own special cell-edit control.
 	//!   - The edit control must inherit from CWnd
 	//!   - The edit control must delete itself when it looses focus
 	//!   - The edit control must send a LVN_ENDLABELEDIT message when edit is complete
+	//!
 	//! @param owner The list control starting edit
 	//! @param nRow The index of the row for the cell to edit
 	//! @param nCol The index of the column for the cell to edit
 	//! @return Pointer to the cell editor to use (NULL if cell edit is not possible)
 	virtual CWnd* OnEditBegin(CGridListCtrlEx& owner, int nRow, int nCol) { return NULL; }
 
-	//! Override OnEditEnd() in case one need to change state after a cell-edit
+	//! Override OnEditEnd() in case one need to change state after a cell-edit.
 	virtual void  OnEditEnd() {}
 
-	//! Override Accept() and update CGridColumnTraitVisitor for new column-trait classes
+	//! Override Accept() and update CGridColumnTraitVisitor for new column-trait classes.
 	//!   - Will enable the use of the visitor-pattern ex. for serialization of column-traits
 	virtual void Accept(CGridColumnTraitVisitor& visitor) {}
 
