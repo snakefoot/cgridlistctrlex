033fca84b431053775fffa72c63054c7bb3c00f0
sweaty1 <sweaty1@hotmail.com>
2009-03-29 00:02:25 +0000

    Activated support for persisting column state

--- DemoApplication/CGridListCtrlExApp.cpp
+++ DemoApplication/CGridListCtrlExApp.cpp
@@ -51,7 +51,7 @@ BOOL CGridListCtrlExApp::InitInstance()
 	// Change the registry key under which our settings are stored
 	// TODO: You should modify this string to be something appropriate
 	// such as the name of your company or organization
-	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
+	SetRegistryKey(_T("CGridListCtrlEx Demo Application"));
 
 	CGridListCtrlExDlg dlg;
 	m_pMainWnd = &dlg;
