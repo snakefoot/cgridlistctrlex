8ca0822178dd9e66ed55bd72ee2dbac1f70cc909
sweaty1 <sweaty1@hotmail.com>
2009-09-30 21:33:35 +0000

    Issue #11 - Editor should only tell parent when there are modifications

--- CGridListCtrlEx/CGridColumnTraitEdit.h
+++ CGridListCtrlEx/CGridColumnTraitEdit.h
@@ -40,11 +40,13 @@ public:
 protected:
 	afx_msg void OnKillFocus(CWnd *pNewWnd);
 	afx_msg void OnNcDestroy();
+	afx_msg void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
 	virtual	BOOL PreTranslateMessage(MSG* pMsg);
 
 	int		m_Row;					//!< The index of the row being edited
 	int		m_Col;					//!< The index of the column being edited
 	bool	m_Completed;			//!< Ensure the editor only reacts to a single close event
+	bool	m_Modified;				//!< Register if text was modified while the editor was open
 
 	DECLARE_MESSAGE_MAP();
 };
