a57994e5b21fe67aae92179a348a4ce3c1d44e1a
sweaty1 <sweaty1@hotmail.com>
2011-02-09 22:12:45 +0000

    Renamed CGridColumnConfig to CViewConfigSection (Updated version info)

--- CGridListCtrlEx/CGridListCtrlEx.h
+++ CGridListCtrlEx/CGridListCtrlEx.h
@@ -7,7 +7,8 @@
 // Version: 1.8.1
 //
 // Change History:
-//  1.8.1 - Changed CGridColumnTrait::OnSortRows() to take LVITEM as parameter (2010-11-15)
+// 	1.8.1 - Changed CGridColumnTrait::OnSortRows() to take LVITEM as parameter (2010-11-15)
+//		  Renamed CGridColumnConfig to CViewConfigSection
 //	1.8 - Added checkbox support for all column editor types (2010-10-01)
 //		  Added checkbox toggle for all selected rows
 //		  Added min and max width for columns
