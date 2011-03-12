bdcb57b8953cd7a15e1b96f015412d5432bf1128
sweaty1 <sweaty1@hotmail.com>
2011-03-12 21:19:54 +0000

    Added New BSD License Comment

--- CGridListCtrlEx/CGridListCtrlGroups.cpp
+++ CGridListCtrlEx/CGridListCtrlGroups.cpp
@@ -1,3 +1,9 @@
+//------------------------------------------------------------------------
+// Author:  Rolf Kristensen	
+// Source:  http://www.codeproject.com/KB/list/CGridListCtrlEx.aspx
+// License: Free to use for all (New BSD License)
+//------------------------------------------------------------------------
+
 #include "stdafx.h"
 #include "CGridListCtrlGroups.h"
 
