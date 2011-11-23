59c861eaa4c940221703c82db94c8be49700c0e5
sweaty1 <sweaty1@ea00157d-1f56-0410-8c63-f1771a551224>
2011-11-23 21:34:45 +0000

    Fixed compiler warnings about unreferenced parameters

--- trunk/CGridListCtrlEx/ViewConfigSection.cpp
+++ trunk/CGridListCtrlEx/ViewConfigSection.cpp
@@ -557,6 +557,8 @@ CViewConfigSectionDefault::CViewConfigSectionLocal& CViewConfigSectionDefault::C
 //------------------------------------------------------------------------
 CString CViewConfigSectionDefault::CViewConfigSectionLocal::ReadSetting(const CString& strSection, const CString& strSetting, const CString& strDefval) const
 {
+	strSection;	// Avoid compiler warning
+
 	for(int i = 0; i < m_LocalSettings.GetSize(); ++i)
 		if (m_LocalSettings.GetKeyAt(i)==strSetting)
 			return m_LocalSettings.GetValueAt(i);
@@ -573,6 +575,7 @@ CString CViewConfigSectionDefault::CViewConfigSectionLocal::ReadSetting(const CS
 //------------------------------------------------------------------------
 void CViewConfigSectionDefault::CViewConfigSectionLocal::WriteSetting(const CString& strSection, const CString& strSetting, const CString& strValue)
 {
+	strSection;	// Avoid compiler warning
 	m_LocalSettings.Add(strSetting, strValue);
 }
 
@@ -583,6 +586,7 @@ void CViewConfigSectionDefault::CViewConfigSectionLocal::WriteSetting(const CStr
 //------------------------------------------------------------------------
 void CViewConfigSectionDefault::CViewConfigSectionLocal::RemoveSection(const CString& strSection)
 {
+	strSection;	// Avoid compiler warning
 	m_LocalSettings.RemoveAll();
 }
 
