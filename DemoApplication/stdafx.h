9d8cad95fe36a42a28e41cf309337253945bae80
sweaty1 <sweaty1@hotmail.com>
2012-04-30 18:59:56 +0000

    Disable compiler warnings introduced by MFC

--- DemoApplication/stdafx.h
+++ DemoApplication/stdafx.h
@@ -4,8 +4,6 @@
 
 #pragma once
 
-#pragma warning(disable:4100)	// unreferenced formal parameter
-
 #if _MSC_VER >= 1400
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
@@ -38,10 +36,35 @@
 #endif
 
 
+#ifndef UNICODE
+#define CGRIDLISTCTRLEX_GROUPMODE
+#endif
+
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS	// some CString constructors will be explicit
 
-// turns off MFC's hiding of some common and often safely ignored warning messages
-#define _AFX_ALL_WARNINGS
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+
+#pragma warning( disable:4514 )	// warning C4514: unreferenced inline function has been remove
+#pragma warning( disable:4710 )	// warning C4710: function not inlined
+#pragma warning( disable:4711 )	// warning C4711: function selected for automatic inline expansion
+#pragma warning( disable:4820 )	// warning C4820: X bytes padding added after data member
+
+#pragma warning( push )
+#pragma warning( disable:4812 )	// warning C4812: obsolete declaration style: please use '_CIP<_Interface,_IID>::_CIP' instead
+#pragma warning( disable:4005 )	// warning C4005: '_WIN32_WINDOWS' : macro redefinition
+#pragma warning( disable:4668 )	// warning C4668: '__midl' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
+#pragma warning( disable:4820 )	// warning C4820: X bytes padding added after data member
+#pragma warning( disable:4619 )	// warning C4619: #pragma warning : there is no warning number
+#pragma warning( disable:4625 )	// warning C4625: copy constructor could not be generated because a base class copy constructor is inaccessible
+#pragma warning( disable:4626 )	// warning C4626: assignment operator could not be generated because a base class assignment operator is inaccessible
+#pragma warning( disable:4365 )	// warning C4365: '=' : conversion from 'int' to 'size_t', signed/unsigned mismatch
+#pragma warning( disable:4244 )	// warning C4244: 'return' : conversion from 'const time_t' to 'LONG_PTR', possible loss of data
+#pragma warning( disable:4263 )	// warning C4263: member function does not override any base class virtual member function
+#pragma warning( disable:4264 )	// warning C4264: no override available for virtual member function from base; function is hidden
+#pragma warning( disable:4917 )	// warning C4917: a GUID can only be associated with a class, interface or namespace
+#pragma warning( disable:4555 )	// warning C4555: expression has no effect; expected expression with side-effect
+#pragma warning( disable:4640 )	// warning C4640: construction of local static object is not thread-safe
+#pragma warning( disable:4571 )	// warning C4571: Informational: catch(...) semantics changed since Visual C++ 7.1; structured exceptions (SEH) are no longer caught
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
@@ -53,6 +76,8 @@
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 #include <atlbase.h>
+
+#pragma warning( pop )
 
 #include <string>
 #include <vector>
