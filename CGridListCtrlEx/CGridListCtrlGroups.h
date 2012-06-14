99354d84d7c2295110a58298f9bc636b17299812
sweaty1 <sweaty1@hotmail.com>
2012-06-14 18:57:12 +0000

    Added fall back solution for enabling sorting of groups after having reordered columns

--- CGridListCtrlEx/CGridListCtrlGroups.h
+++ CGridListCtrlEx/CGridListCtrlGroups.h
@@ -74,6 +74,7 @@ protected:
 	virtual afx_msg BOOL OnGetEmptyMarkup(NMHDR* pNMHDR, LRESULT* pResult);
 	virtual afx_msg void OnPaint();
 	virtual afx_msg LRESULT OnRemoveAllGroups(WPARAM wParam, LPARAM lParam);
+	virtual afx_msg BOOL OnHeaderEndDrag(UINT, NMHDR* pNmhdr, LRESULT* pResult);
 	//}}AFX_MSG
 
 	int m_GroupCol;		//!< Rows are grouped according to this column
